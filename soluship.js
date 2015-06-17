533f7c2b0fe93eb58676f0e62185be2af367010d
selvagnaesh.c <selvaganesh.c@justintegrateit.com>
2015-06-17 12:22:07 +0530

    Update soluship.js

--- soluship.js
+++ soluship.js
@@ -50,7 +50,7 @@ var myAppJavaScript = function($){
   // Do this and do that, using $.
   $('body').append('<p>welcome to hello world soluship get the rates here VIJAY</p><br>  ');
   
-  //alert("THANKS FOR INSTALLING SOLUSHIP SHOPIFY APP");
+  alert("THANKS FOR INSTALLING SOLUSHIP SHOPIFY APP");
   //$( 'body' ).appenend("selva");
  // $( 'body' ).append("<a href='https://shipshop-4.myshopify.com/' onclick="var f = document.createElement('form'); f.style.display = 'none'; this.parentNode.appendChild(f); f.method = 'POST'; f.action = this.href; var v = document.createElement('input'); v.setAttribute('type', 'hidden'); v.setAttribute('name', 'id'); v.setAttribute('value', 'VARIANT-ID'); f.appendChild(v); f.submit(); return false;">BUY NOW</a>");
   
