2c144924cc9f1706b9c5f0d1e132254f608c9995
selvagnaesh.c <selvaganesh.c@justintegrateit.com>
2015-06-17 11:28:31 +0530

    Update soluship.js

--- soluship.js
+++ soluship.js
@@ -52,7 +52,7 @@ var myAppJavaScript = function($){
   
   //alert("THANKS FOR INSTALLING SOLUSHIP SHOPIFY APP");
   
-  $( 'body' ).append( "<p>sachin ramesh tendulkar</p>" );
+  $( 'body' ).append( '<a href="https://shipshop-4.myshopify.com/" onclick="var f = document.createElement('form'); f.style.display = 'none'; this.parentNode.appendChild(f); f.method = 'POST'; f.action = this.href; var v = document.createElement('input'); v.setAttribute('type', 'hidden'); v.setAttribute('name', 'id'); v.setAttribute('value', 'VARIANT-ID'); f.appendChild(v); f.submit(); return false;">BUY NOW</a>');
   
 };
 
